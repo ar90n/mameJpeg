1853e2de5f14d68ef0b186a65ec161747c37b8b8
Masahiro Wada <argon.argon.argon@gmail.com>
2015-03-24 01:06:37 -0400

    add some codes

--- mameJpeg.h
+++ mameJpeg.h
@@ -7,8 +7,10 @@ extern "C" {
 
 #include <ctype.h>
 
-#define CHECK( X ) do{if( (X) == false ) return false; } while(0);
-#define NULL_CHECK( X )  CHECK( (X) != NULL )
+#define CHECK_PARAM( X ) do{if( (X) == false ) return false; } while(0);
+#define NULL_CHECK_PARAM( X )  CHECK_PARAM( (X) != NULL )
+#define CHECK_PROC( LABEL, X ) do{ if( (X) == false ) goto LABEL; } while(0);
+
 
 typedef struct {
     uint8_t* buffer;
@@ -20,7 +22,7 @@ typedef struct {
 
 bool mameBitstream_initialize( mameBitstream_context* context, uint8_t* buffer, size_t buffer_length )
 {
-    NULL_CHECK( buffer )
+    NULL_CHECK_PARAM( buffer )
 
     context->buffer = buffer;
     context->buffer_length = buffer_length
@@ -33,9 +35,9 @@ bool mameBitstream_initialize( mameBitstream_context* context, uint8_t* buffer, 
 
 bool mameBitstream_readByte( mameBitstream_context* context )
 {
-    NULL_CHECK( context )
-    CHECK( ( context->pos + 1 ) < context->buffer_length )
-    CHECK( context->cache_use_bits < 8 )
+    NULL_CHECK_PARAM( context )
+    CHECK_PARAM( ( context->pos + 1 ) < context->buffer_length )
+    CHECK_PARAM( context->cache_use_bits < 8 )
 
     context->cache = ( context->cache << 8 ) | ( context->buffer[ context->pos ] );
     context->cache_use_bits += 8;
@@ -46,9 +48,9 @@ bool mameBitstream_readByte( mameBitstream_context* context )
 
 bool mameBitstream_readBits( mameBitstream_context* context, void* buffer, int buffer_length, int bits )
 {
-    NULL_CHECK( context )
-    NULL_CHECK( buffer )
-    CHECK( bits <= ( 8 * buffer_length ) )
+    NULL_CHECK_PARAM( context )
+    NULL_CHECK_PARAM( buffer )
+    CHECK_PARAM( bits <= ( 8 * buffer_length ) )
 
     int buffer_pos = 0;
     while( 0 < bits )
@@ -74,24 +76,152 @@ bool mameBitstream_readBits( mameBitstream_context* context, void* buffer, int b
 }
 
 
+typedef enum {
+    MAMEJPEG_ENCODE = 0,
+    MAMEJPEG_DECODE = 1,
+} mameJpeg_mode;
 
-
-
-
-
-
-
-
-
-
+typedef enum{
+    MAMEJPEG_SOI = 0xffd8,
+    MAMEJPEG_APP0 = 0xffe0,
+    MAMEJPEG_DQT = 0xffdb,
+    MAMEJPEG_SOF0 = 0xffc0,
+    MAMEJPEG_DHT = 0xffc4,
+    MAMEJPEG_SOS = 0xffda,
+    MAMEJPEG_EOI = 0xffd9,
+} mameJpeg_marker;
 
 
 typedef struct {
+    uint8_t* work_buffer;
+    size_t work_buffer_length;
+    uint8_t* io_ptr;
+    size_t io_size;
+    mameBitstream_context io_stream[1];
+    mameJpeg_mode mode;
+    int progress;
 } mameJpeg_context;
 
+bool mameJpeg_getImageSize( void* input, int* width, int* height )
+{
+    width = height = 100;
+    return true;
+}
 
+bool mameJpeg_getBufferSize( int width, int height )
+{
+    return ( width * height * 3 ) >> 1;
+}
 
+bool mameJpeg_getProgress( mameJpeg_context* context, int* progress )
+{
+    *progress = context->progress;
 
+    return true;
+}
+
+bool mameJpeg_initialize( mameJpeg_context* context,
+                          void* work_buffer,
+                          size_t work_buffer_length,
+                          void* input_ptr,
+                          size_t input_size,
+                          void* output_ptr,
+                          size_t output_size,
+                          mameJpeg_mode mode )
+{
+    NULL_CHECK_PARAM( context )
+    NULL_CHECK_PARAM( work_buffer )
+    CHECK_PARAM( 0 < work_buffer_length )
+    NULL_CHECK_PARAM( input_ptr )
+    CHECK_PARAM( input_size )
+    NULL_CHECK_PARAM( output_ptr )
+    CHECK_PARAM( output_size )
+    CHECK_PARAM( mode == MAMEJPEG_ENCODE || mode == MAMEJPEG_DECODE )
+
+    context->work_buffer = (uint8_t*)work_buffer;
+    context->work_buffer_length = work_buffer_length;
+
+    context->mode = mode;
+    context->io_ptr =  ( mode == MAMEJPEG_ENCODE ) ? input_ptr : output_ptr;
+    context->io_size = ( mode == MAMEJPEG_ENCODE ) ? input_size : output_size;
+    mameBitstream_initialize( context->io_stream,
+                              ( mode == MAMEJPEG_ENCODE ) ? output_ptr : input_ptr,
+                              ( mode == MAMEJPEG_ENCODE ) ? output_ptr : input_ptr );
+
+    return true;
+}
+
+bool mameJpeg_Decode( mameJpeg_context* context )
+{
+    NULL_CHECK_PARAM( context )
+    CHECK_PARAM( context->mode == MAMEJPEG_DECODE )
+
+    mameJpeg_marker marker;
+    while( mameJpeg_getNextMarker( context, &marker ) )
+    {
+        switch( marker )
+        {
+            case MAMEJPEG_SOI:
+            {
+            }break;
+            case MAMEJPEG_APP0:
+            {
+            }break;
+            case MAMEJPEG_DQT:
+            {
+            }break;
+            case MAMEJPEG_SOF0:
+            {
+            }break;
+            case MAMEJPEG_DHT:
+            {
+            }break;
+            case MAMEJPEG_SOS:
+            {
+            }break;
+            case MAMEJPEG_EOI:
+            {
+            }break;
+            default:
+            {
+            }break;
+        }
+    }
+
+    return true;
+}
+
+bool mameJpeg_getNextMarker( mameJpeg_context* context, mameJpeg_marker* marker )
+{
+    uint8_t prefix;
+    while( mameBitstream_readBits( context->io_stream, &prefix, 8 ) )
+    {
+        if( prefix == 0xff )
+        {
+            uint8_t val;
+            CHECK_PROC( END_LOOP, mameBitstream_readBits( context->io_stream, &val, 8 ) );
+
+            *marker = ( prefix << 8 ) | val;
+            return true;
+        }
+    }
+END_LOOP:
+
+    return false;
+}
+
+bool mameJpeg_Encode( mameJpeg_context* context )
+{
+    NULL_CHECK_PARAM( context )
+    CHECK_PARAM( context->mode == MAMEJPEG_ENCODE )
+
+    return true;
+}
+
+bool mameJpeg_dispose( mameJpeg_context* context )
+{
+    return true;
+}
 
 # ifdef __cplusplus
 }
